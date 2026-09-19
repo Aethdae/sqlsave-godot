0ad1bcc21d9932b25a8ec59ebc57e6c8339043ea
Tyler Long <aethdae@gmail.com>
2026-09-19 00:42:45 -0500

    init commit, setup manager and modular GameSave class

---
+++ SqlSave/SaveManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.Sqlite;
+using Godot;
+
+namespace SqlSave
+{
+    public class SaveManager
+    {
+        public static SqliteConnection? Connection { get; set; }
+
+
+        public SaveManager()
+        {
+            string userData = OS.GetUserDataDir();
+            Connection = new SqliteConnection(userData + "/save/Save.sav");
+
+        }
+    }
+}
