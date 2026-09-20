3c38bdc5f03a17a58c9f3038d83f392ba09282c5
Tyler Long <aethdae@gmail.com>
2026-09-20 03:59:16 -0500

    small tests complete

--- SqlSave/SaveManager.cs
+++ SqlSave/SaveManager.cs
@@ -11,7 +11,29 @@ namespace SqlSave
         public SaveManager()
         {
             string userData = OS.GetUserDataDir();
-            Connection = new SqliteConnection(userData + "/save/Save.sav");
+            Connection = new SqliteConnection(userData + "/saves/Save.sav");
+            Connection.Open();
+
+            using var command = Connection.CreateCommand();
+            command.CommandText = """
+            CREATE TABLE IF NOT EXISTS saves(
+            id INTEGER AUTOINCREMENT PRIMARY KEY
+            )
+            """;
+            using var reader = command.ExecuteReader();
+
+            GD.Print("Trying to read..");
+            if (reader.Read())
+            {
+                var spell = reader.GetValue("spell");
+                return (string)spell;
+            }
+            return null;
+
+        }
+
+        private void Init()
+        {
 
         }
     }
